035087f36ed50b102f9ec1be5d8dd067148f90ae
Robert Heath <rheath@maprtech.com>
2015-09-29 14:51:28 -0700

    Update Ex02MapRdbAdmin.java
    Removed a reference to a bug that has been fixed.

--- src/main/java/com/mapr/db/samples/basic/Ex02MapRdbAdmin.java
+++ src/main/java/com/mapr/db/samples/basic/Ex02MapRdbAdmin.java
@@ -101,7 +101,7 @@ public class Ex02MapRdbAdmin {
 
   private void createAndReadDocuments() throws InterruptedException {
     table = MapRDB.getTable(TABLE_PATH);
-    table.setOption(Table.TableOption.BUFFERWRITE, true); // write synchronously (better naming/value see bug 20255)
+    table.setOption(Table.TableOption.BUFFERWRITE, true); 
 
     Document meta = Json.newDocument()
             .set("title", "Home Page")
